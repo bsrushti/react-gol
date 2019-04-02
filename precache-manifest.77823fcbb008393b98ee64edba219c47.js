0fcb373f14ce2756ba5785b1020cf238b73b42ca
bsrushti <bsrushti@in-bsrushti.Dlink>
2019-04-02 19:52:30 +0530

    Updates

---
+++ precache-manifest.77823fcbb008393b98ee64edba219c47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b46a32cffa792dba7918",
+    "url": "/react-gol/static/css/main.36fc555c.chunk.css"
+  },
+  {
+    "revision": "b46a32cffa792dba7918",
+    "url": "/react-gol/static/js/main.1fd962f0.chunk.js"
+  },
+  {
+    "revision": "42351f58b146a9746e6f",
+    "url": "/react-gol/static/js/runtime~main.68b246ae.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/react-gol/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "de335e26c7afea0a6a114175723365bd",
+    "url": "/react-gol/index.html"
+  }
+];
